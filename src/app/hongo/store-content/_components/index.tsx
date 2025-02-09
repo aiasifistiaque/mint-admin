eea1af1cbdf7cf14a79d3043359d94fb88e54be7
Asif Istiaque <asifistiaque.ai@gmail.com>
2025-02-10 05:30:25 +0600

    update

--- src/app/hongo/store-content/_components/index.tsx
+++
@@ -1,8 +0,0 @@
-export type HomeContentProps = {
-	content: any;
-	dataModel: any;
-};
-
-export { default as StoreConfig } from './StoreConfig';
-
-export { default as storeData } from './storeData';
