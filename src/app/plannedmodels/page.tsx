249ef20c99229757b805e8a8046d8f67436f5798
Asif Istiaque <asifistiaque.ai@gmail.com>
2025-03-02 19:44:04 +0600

    s

--- src/app/plannedmodels/page.tsx
+++ src/app/plannedmodels/page.tsx
@@ -4,22 +4,22 @@ import React from 'react';
 import { NextPage } from 'next';
 import { BackendPageTable, BackendTableObjectProps } from '@/components/library';
 
-const fields = ['name', 'description', 'status', 'stack', 'createdAt'];
-const tableFields = ['name', 'status', 'stack', 'createdAt'];
+const fields = ['name', 'description', 'project', 'createdAt'];
+const tableFields = ['name', 'description', 'project', 'createdAt'];
 
 const formFields = [
 	{
-		sectionTitle: 'Project Plan',
-		fields: ['name', ['platform', 'status', 'stack']],
+		sectionTitle: 'Model Overview',
+		fields: ['name', 'project', 'description'],
 	},
 ];
 
 const table: BackendTableObjectProps = {
-	title: 'Plan Project',
-	path: 'plannedprojects',
+	title: 'Plan Model',
+	path: 'plannedmodels',
 	export: false,
 	button: {
-		title: 'New Project',
+		title: 'New model',
 		isModal: true,
 		layout: formFields,
 	},
@@ -34,6 +34,10 @@ const table: BackendTableObjectProps = {
 			title: 'Edit',
 			type: 'edit-modal',
 			layout: formFields,
+		},
+		{
+			title: 'Duplicate',
+			type: 'duplicate',
 		},
 		{
 			title: 'Delete',
