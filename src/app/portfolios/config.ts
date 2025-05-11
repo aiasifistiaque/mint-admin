7a9ce8c068fef20a63c12e0c4c9e702de9c84fca
Asif Istiaque <asifistiaque.ai@gmail.com>
2025-05-11 23:29:17 +0600

    s

--- src/app/portfolios/config.ts
+++ src/app/portfolios/config.ts
@@ -12,7 +12,57 @@ export const fields = [
 export const formFields = [
 	{
 		sectionTitle: 'Project Details',
-		fields: ['image', 'name', ['category', 'status'], 'liveUrl', 'priority', 'isFeatured'],
+		fields: [
+			'image',
+
+			'name',
+			['category', 'status'],
+			'priority',
+
+			['liveUrl', 'showLiveUrl'],
+			'isFeatured',
+		],
+	},
+	{
+		sectionTitle: 'Case Study',
+		fields: ['coverImage', 'title', 'showCaseStudy', 'subTitle', 'shortDescription', 'overview'],
+	},
+	{
+		sectionTitle: 'Case Study: Project Overview',
+		fields: ['logo', 'client', ['duration', 'year'], 'productTitle', 'productDescription', 'tags'],
+	},
+	{
+		sectionTitle: 'Case Study: Project Images',
+		fields: ['images'],
+	},
+	{
+		sectionTitle: 'Company Details',
+		fields: [
+			'companyName',
+			['companyCategory', 'companyUrl'],
+			'companyTitle',
+			'companyDescription',
+		],
+	},
+	{
+		sectionTitle: 'The Challenge',
+		fields: ['challengeTitle', 'challengeDescription'],
+	},
+	{
+		sectionTitle: 'The Solution',
+		fields: ['solutionTitle', 'solutionDescription', 'solutionFeatures'],
+	},
+	{
+		sectionTitle: 'Our Approach',
+		fields: ['approachTitle', 'approachDescription'],
+	},
+	{
+		sectionTitle: 'Tech Stack',
+		fields: ['techStackTitle', 'techStackDescription', 'techStack'],
+	},
+	{
+		sectionTitle: 'Client Review',
+		fields: ['review'],
 	},
 ];
 
