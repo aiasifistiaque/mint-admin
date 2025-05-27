440d5f87fb47b94065ca17cf1c538c3b0b4cc5d7
Asif Istiaque <asifistiaque.ai@gmail.com>
2025-05-27 17:52:14 +0600

    mobile play

--- src/components/library/modals/upload-modal/MyPhotos.tsx
+++ src/components/library/modals/upload-modal/MyPhotos.tsx
@@ -2,20 +2,27 @@
 import { useRef, useState } from 'react';
 import { Box, Flex, Grid, Image, useColorModeValue } from '@chakra-ui/react';
 import { useGetAllUploadsQuery } from '@/store/services/uploadApi';
+import { useIsMobile } from '../../hooks';
 
 const MyPhotos = ({ handleSelect, type = 'image' }: { handleSelect: any; type: string }) => {
 	const { data } = useGetAllUploadsQuery({ limit: '999', type, page: 1, sort: '-createdAt' });
 	const [selected, setSelected] = useState<any>(null);
 	const borderColor = useColorModeValue('brand.500', 'brand.200');
 
+	const isMobile = useIsMobile();
+
 	const videoRef = useRef<any>(null);
 
 	const handleMouseEnter = () => {
+		if (isMobile) return;
+		// If the type is not video, we don't need to play the video
 		if (type !== 'video') return;
 		videoRef.current?.play();
 	};
 
 	const handleMouseLeave = () => {
+		if (isMobile) return;
+		// If the type is not video, we don't need to play the video
 		if (type !== 'video') return;
 		videoRef.current?.pause();
 		videoRef.current.currentTime = 0;
@@ -43,6 +50,7 @@ const MyPhotos = ({ handleSelect, type = 'image' }: { handleSelect: any; type: s
 					bg='whitesmoke'>
 					{type == 'video' ? (
 						<video
+							controls={isMobile}
 							muted
 							ref={videoRef}
 							playsInline
