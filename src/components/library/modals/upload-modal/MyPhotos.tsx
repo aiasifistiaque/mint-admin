a8775aaba2fd78272354be320c51766775a0d917
Asif Istiaque <asifistiaque.ai@gmail.com>
2025-05-27 18:29:45 +0600

    s

--- src/components/library/modals/upload-modal/MyPhotos.tsx
+++ src/components/library/modals/upload-modal/MyPhotos.tsx
@@ -1,15 +1,52 @@
 'use client';
-import { useRef, useState } from 'react';
-import { Box, Flex, Grid, Image, useColorModeValue } from '@chakra-ui/react';
+import { useEffect, useRef, useState } from 'react';
+import { Box, Flex, FlexProps, Grid, Image, useColorModeValue } from '@chakra-ui/react';
 import { useGetAllUploadsQuery } from '@/store/services/uploadApi';
 import { useIsMobile } from '../../hooks';
 
 const MyPhotos = ({ handleSelect, type = 'image' }: { handleSelect: any; type: string }) => {
 	const { data } = useGetAllUploadsQuery({ limit: '999', type, page: 1, sort: '-createdAt' });
 	const [selected, setSelected] = useState<any>(null);
-	const borderColor = useColorModeValue('brand.500', 'brand.200');
+
+	return (
+		<Grid
+			gridTemplateColumns={{ base: '1fr 1fr', md: 'repeat(5, 1fr)' }}
+			gap={2}>
+			{data?.doc?.map((item: any, i: number) => (
+				<ImageComponent
+					src={item?.url}
+					type={type}
+					onClick={() => {
+						setSelected(item?.url);
+						handleSelect(item?.url);
+					}}
+					selected={selected}
+					key={i}
+				/>
+			))}
+		</Grid>
+	);
+};
+
+const ImageComponent = ({
+	src,
+	type,
+	selected,
+	...props
+}: FlexProps & {
+	src: string;
+	type: string;
+	selected: any;
+}) => {
 	const videoRef = useRef<any>(null);
 	const isMobile = useIsMobile();
+	const borderColor = useColorModeValue('brand.500', 'brand.200');
+
+	useEffect(() => {
+		// if (isMobile) {
+		// 	videoRef?.current?.play();
+		// }
+	}, [videoRef]);
 
 	const handleMouseEnter = () => {
 		if (isMobile) return;
@@ -20,6 +57,7 @@ const MyPhotos = ({ handleSelect, type = 'image' }: { handleSelect: any; type: s
 
 	const handleMouseLeave = () => {
 		if (isMobile) return;
+
 		// If the type is not video, we don't need to play the video
 		if (type !== 'video') return;
 		videoRef.current?.pause();
@@ -27,49 +65,38 @@ const MyPhotos = ({ handleSelect, type = 'image' }: { handleSelect: any; type: s
 	};
 
 	return (
-		<Grid
-			gridTemplateColumns={{ base: '1fr 1fr', md: 'repeat(5, 1fr)' }}
-			gap={2}>
-			{data?.doc?.map((item: any) => (
-				<Flex
-					borderRadius='4px'
-					onClick={() => {
-						setSelected(item?.url);
-						handleSelect(item?.url);
-					}}
-					cursor='pointer'
-					key={item._id}
-					w='full'
-					h='200px'
-					onMouseEnter={handleMouseEnter}
-					onMouseLeave={handleMouseLeave}
-					border='2px solid'
-					borderColor={selected === item?.url ? borderColor : '#ddd'}
-					bg='whitesmoke'>
-					{type == 'video' ? (
-						<video
-							muted
-							poster={`${item?.url}?x-oss-process=video/snapshot,t_0,f_jpg,w_0,h_0,m_fast`} // Add thumbnail
-							ref={videoRef}
-							playsInline={!isMobile}
-							loop
-							style={{ width: '100%', height: '100%', objectFit: 'contain' }}>
-							<source
-								src={item?.url}
-								type='video/mp4'
-							/>
-							Your browser does not support the video tag.
-						</video>
-					) : (
-						<Image
-							objectFit='contain'
-							src={item?.url}
-							alt={item?.name}
-						/>
-					)}
-				</Flex>
-			))}
-		</Grid>
+		<Flex
+			borderRadius='4px'
+			cursor='pointer'
+			w='full'
+			h='200px'
+			onMouseEnter={handleMouseEnter}
+			onMouseLeave={handleMouseLeave}
+			border='2px solid'
+			borderColor={selected === src ? borderColor : '#ddd'}
+			bg='whitesmoke'
+			{...props}>
+			{type == 'video' ? (
+				<video
+					muted
+					ref={videoRef}
+					playsInline
+					loop
+					style={{ width: '100%', height: '100%', objectFit: 'contain' }}>
+					<source
+						src={src}
+						type='video/mp4'
+					/>
+					Your browser does not support the video tag.
+				</video>
+			) : (
+				<Image
+					objectFit='contain'
+					src={src}
+					alt={src}
+				/>
+			)}
+		</Flex>
 	);
 };
 
