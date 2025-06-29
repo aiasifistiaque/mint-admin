f9e1a6463474f3cba82216138eb7a14297e6c1a7
Asif Istiaque <asifistiaque.ai@gmail.com>
2025-06-30 04:22:26 +0600

    media

---
+++ src/app/files/page.tsx
@@ -0,0 +1,22 @@
+'use client';
+import React from 'react';
+import { NextPage } from 'next';
+import { ServerPageTable, ServerTableObjectProps } from '@/components/library';
+
+const table: ServerTableObjectProps = {
+	title: 'File Management',
+	path: 'files',
+
+	menu: [
+		{ type: 'view-server-modal', title: 'View' },
+		{ type: 'view-item', title: 'Go To Post' },
+
+		{ type: 'delete', title: 'Delete' },
+	],
+};
+
+const BrandPage: NextPage = () => {
+	return <ServerPageTable table={table} />;
+};
+
+export default BrandPage;
